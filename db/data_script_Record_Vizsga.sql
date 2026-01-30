f838e8cb8a84e40691399b00e612ae036ab78da3
Halmai Bence <148640182+Bobiyi@users.noreply.github.com>
2026-01-30 20:59:23 +0100

    Code cleanup

--- db/data_script_Record_Vizsga.sql
+++ db/data_script_Record_Vizsga.sql
@@ -10,29 +10,32 @@ insert into `user`(id,`name`,email,phone)
 
 /*------------------------------------------- SZERZŐK ------------------------------------------------------*/
 insert into artist(`name`,active_since,nationality,`url`,is_group)
-    values("Metallica","1981","usa","https://www.metallica.com",1),
-    values("Michael Jackson","1964","usa","https://www.michaeljackson.com",0),
-    values("Playboi Carti","2015","usa","https://shop.playboicarti.com/",0)
+    values
+        ("Metallica","1981","usa","https://www.metallica.com",1),
+        ("Michael Jackson","1964","usa","https://www.michaeljackson.com",0),
+        ("Playboi Carti","2015","usa","https://shop.playboicarti.com/",0)
     ;
 /*------------------------------------------- ALBUMOK ------------------------------------------------------*/
 insert into `record`(`name`,type_id,release_date,`length`)
-    values("Master Of Dolls",1,"1986",8),
-    values("Ride The Storm",1,"1984",64),
-    values("Restock",1,"1997",13),
-    values("Good",1,"1987",9),
-    values("Freindly",1,"1991",9),
-    values("Whole Lotta Blue",1,"2020",24),
-    values("Live Lit",1,"2018",19),
-    values("Melody",1,"2025",30)
+    values
+        ("Master Of Dolls",1,"1986",8),
+        ("Ride The Storm",1,"1984",64),
+        ("Restock",1,"1997",13),
+        ("Good",1,"1987",9),
+        ("Freindly",1,"1991",9),
+        ("Whole Lotta Blue",1,"2020",24),
+        ("Live Lit",1,"2018",19),
+        ("Melody",1,"2025",30)
     ;
 /*------------------------------------------- SZERZŐK - ALBUMOK KÖTÉS---------------------------------------*/
 insert into artist_record(artist_id,record_id,`role`)
-    values(1,1,"producer"),
-    values(1,2,"producer"),
-    values(1,3,"producer"),
-    values(2,4,"producer"),
-    values(2,5,"producer"),
-    values(3,6,"producer"),
-    values(3,7,"producer"),
-    values(3,8,"producer"),
+    values
+        (1,1,"producer"),
+        (1,2,"producer"),
+        (1,3,"producer"),
+        (2,4,"producer"),
+        (2,5,"producer"),
+        (3,6,"producer"),
+        (3,7,"producer"),
+        (3,8,"producer"),
     ;
