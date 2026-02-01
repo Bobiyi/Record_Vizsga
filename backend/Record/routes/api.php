c7b51f0e221e031e505e900631639444cfc3d70a
Halmai Bence <148640182+Bobiyi@users.noreply.github.com>
2026-02-01 16:06:03 +0100

    added more artists + records
    Masayoshi Takanaka
    Kárpátia
    Korda György
    
    + added hashed admin password

--- backend/Record/routes/api.php
+++ backend/Record/routes/api.php
@@ -1,3 +1,5 @@
 <?php
 
+use App\Http\Controllers\RecordController;
 use Illuminate\Support\Facades\Route;
+
--- db/data_script_Record_Vizsga.sql
+++ db/data_script_Record_Vizsga.sql
@@ -8,15 +8,18 @@ insert into record_type(type_name)
 
 /*------------------------------------------- ADMIN FELHASZNÁLÓ -------------------------------------------*/
 insert into `user`(id,password_hash,`name`,email,phone)
-    values(0,"admin","admin","admin@record.hu","06309353729");
+    values(0,"admin","$2y$12$ETL70RA2OEi6gZcCLUb03u8xapsiDTmk96NpCq2k.xL9ckxJhW0BG","admin@record.hu","06309353729"); /*DO NOT REPLACE PASSWORD IT IS FINE*/
 
 /*------------------------------------------- SZERZŐK ------------------------------------------------------*/
 insert into artist(`name`,active_since,nationality,`url`,is_group)
     values
         ("Metallica","1981","usa","https://www.metallica.com",1),
         ("Michael Jackson","1964","usa","https://www.michaeljackson.com",0),
-        ("Playboi Carti","2015","usa","https://shop.playboicarti.com/",0),
-        ("1001 Eyes", "2021", null, null, 0)
+        ("Playboi Carti","2015","usa",null,0),
+        ("1001 Eyes", "2021", null, null, 0),
+        ("Masayoshi Takanaka","1970","jpn","https://takanaka.com",0),
+        ("Kárpátia","2003","hun","https://www.karpatiazenekar.hu",0),
+        ("Korda György","1958","hun",null,0)
     ;
 /*------------------------------------------- ALBUMOK ------------------------------------------------------*/
 insert into `record`(`name`,type_id,release_year,`length`)
@@ -31,7 +34,14 @@ insert into `record`(`name`,type_id,release_year,`length`)
         ("Melody",1,"2025",30),
         ("1001 Eyes", 1, "2021", 13),
         ("Singularity", 1, "2024", 15),
-        ("SIGNAL: MEMORIES", 1, "2024", 7)
+        ("SIGNAL: MEMORIES", 1, "2024", 7),
+        ("THE RAINBOW SKELETONS",1,"1981",14),
+        ("LEG DANCIN'",2,"2006",4),
+        ("BRASILIAN LAND",1,"1978",8),
+        ("AL OF YOU",1,"2006",14),
+        ("Merészeké a szerencse",1,"2014",11),
+        ("Az Üldözött",1,"2013",13),
+        ("Napsütés kell a világnak - Tegnap és ma",1,"2005",15)
     ;
 /*------------------------------------------- SZERZŐK - ALBUMOK KÖTÉS---------------------------------------*/
 insert into artist_record(artist_id,record_id,`role`)
@@ -46,6 +56,12 @@ insert into artist_record(artist_id,record_id,`role`)
         (3,8,"producer"),
         (4,9,"producer"),
         (4,10,"producer"),
-        (4,11,"producer")
-
+        (4,11,"producer"),
+        (5,12,"producer"),
+        (5,13,"producer"),
+        (5,14,"producer"),
+        (5,15,"producer"),
+        (6,16,"producer"),
+        (6,17,"producer"),
+        (7,18,"producer")
     ;
