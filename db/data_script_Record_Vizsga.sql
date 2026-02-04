74db811507b03ac248fcf46aa96fbb54b77db09c
HokiMester <erikadolf201@gmail.com>
2026-02-04 21:16:23 +0100

    theNBHD data

--- db/data_script_Record_Vizsga.sql
+++ db/data_script_Record_Vizsga.sql
@@ -16,10 +16,11 @@ insert into artist(`name`,active_since,nationality,`url`,is_group)
         ("Metallica","1981","usa","https://www.metallica.com",1),
         ("Michael Jackson","1964","usa","https://www.michaeljackson.com",0),
         ("Playboi Carti","2015","usa",null,0),
-        ("1001 Eyes", "2021", null, null, 0),
+        ("1000 Eyes", "2021", null, null, 0),
         ("Masayoshi Takanaka","1970","jpn","https://takanaka.com",0),
         ("Kárpátia","2003","hun","https://www.karpatiazenekar.hu",0),
-        ("Korda György","1958","hun",null,0)
+        ("Korda György","1958","hun",null,0),
+        ("The Neighbourhood", "2011", "usa", "https://tour.thenbhd.com",1)
     ;
 /*------------------------------------------- ALBUMOK ------------------------------------------------------*/
 insert into `record`(`name`,type_id,release_year,`length`)
@@ -32,7 +33,7 @@ insert into `record`(`name`,type_id,release_year,`length`)
         ("Whole Lotta Blue",1,"2020",24),
         ("Live Lit",1,"2018",19),
         ("Melody",1,"2025",30),
-        ("1001 Eyes", 1, "2021", 13),
+        ("1000 Eyes", 1, "2021", 13),
         ("Singularity", 1, "2024", 15),
         ("SIGNAL: MEMORIES", 1, "2024", 7),
         ("THE RAINBOW SKELETONS",1,"1981",14),
@@ -41,7 +42,10 @@ insert into `record`(`name`,type_id,release_year,`length`)
         ("AL OF YOU",1,"2006",14),
         ("Merészeké a szerencse",1,"2014",11),
         ("Az Üldözött",1,"2013",13),
-        ("Napsütés kell a világnak - Tegnap és ma",1,"2005",15)
+        ("Napsütés kell a világnak - Tegnap és ma",1,"2005",15),
+        ("(((((infraSOUND)))))", 1,2025,15),
+        ("Easy To Imagine The Neigbourhood Changing", 1, 2018, 21),
+        ("You're Welcome,", 3, 2012, 2)
     ;
 /*------------------------------------------- SZERZŐK - ALBUMOK KÖTÉS---------------------------------------*/
 insert into artist_record(artist_id,record_id,`role`)
@@ -63,5 +67,8 @@ insert into artist_record(artist_id,record_id,`role`)
         (5,15,"producer"),
         (6,16,"producer"),
         (6,17,"producer"),
-        (7,18,"producer")
+        (7,18,"producer"),
+        (8,19,"producer"),
+        (8,20,"producer"),
+        (8,21,"producer"),
     ;
